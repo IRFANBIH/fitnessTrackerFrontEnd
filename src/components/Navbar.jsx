a3308e1b3a8352eaae338082980600dfac7e88ce
irfan <ipekusic@yahoo.com>
2022-11-14 14:46:32 -0500

    added NavLinks and buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div id="navbar">
         <h2> Welcome to Fitness Tracker</h2>
+
+        <div id="navigation">
+          <NavLink to="login"> <button> Login/SignUp </button> </NavLink>
+          <NavLink to="activities"><button> Activities </button> </NavLink>
+          <NavLink to="routines"> <button> Routines </button>  </NavLink>
+          <NavLink to="MyRoutines"><button> My Routines </button> </NavLink>
+
+        </div>
         
     
   </div>
