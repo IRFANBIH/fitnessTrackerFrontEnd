a7d4b3a88a41b01c4e34d41dbc89c2565142d7c7
irfan <ipekusic@yahoo.com>
2022-11-15 09:38:16 -0500

    committing my changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,14 @@ import {NavLink} from "react-router-dom"
 const Navbar = () => {
   return (
     <div id="navbar">
-        <h2> Welcome to Fitness Tracker</h2>
+        <h2>  <a href="http://localhost:3000/">Welcome to Fitness Tracker</a></h2>
+       
 
         <div id="navigation">
           <NavLink to="login"> <button> Login/SignUp </button> </NavLink>
-          <NavLink to="activities"><button> Activities </button> </NavLink>
-          <NavLink to="routines"> <button> Routines </button>  </NavLink>
-          <NavLink to="MyRoutines"><button> My Routines </button> </NavLink>
+          <NavLink to="activities" className="routes">Activities </NavLink>
+          <NavLink to="routines" className="routes"> Routines  </NavLink>
+          <NavLink to="MyRoutines" className="routes">My Routines </NavLink>
 
         </div>
         
