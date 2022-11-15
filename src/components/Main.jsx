c161abeb197dd991466b74b5ed4296242bbf41f6
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-15 08:08:58 -0600

    created single routine component and made async function for getting all the public routines.  routines are  now being displayed.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,32 @@
-import React from "react"
-import { Navbar, Routines, Activities, MyRoutines} from "./"
+import React, {useState, useEffect} from "react"
+import { Navbar, Routines, Activities, MyRoutines, SingleRoutine} from "./"
+import { getAllPublicRoutines } from "../api"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 const Main = () => {
+
+  const [allRoutines, setAllRoutines] = useState([])
+
+
+
+useEffect(() => {
+  async function getPublicRoutines() {
+    const publicRoutines = await getAllPublicRoutines()
+    setAllRoutines(publicRoutines)
+  }
+  getPublicRoutines()
+}, []);
+
+
+
   return (
     <BrowserRouter>
       <div id="Home">
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Routines />} />
-          <Route exact path="/activities" element={<Activities/>} />
-          <Route exact path="/myRoutines" element={<MyRoutines/>}/>
+          <Route  path="/routines" element={<Routines allRoutines={allRoutines}/>} />
+          <Route  path="/activities" element={<Activities/>} />
+          <Route  path="/myRoutines" element={<MyRoutines/>}/>
         </Routes>
       </div>
     </BrowserRouter>
